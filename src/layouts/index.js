9f0710f22909369f6a37e4d8ec55ac670c1f23e8
Noah Glusenkamp <noah.glusenkamp@gmail.com>
2017-09-15 16:07:31 -0700

    style app layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,32 +1,44 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
 
 class Template extends React.Component {
   render() {
-    const { location, children } = this.props
-    let header
-    if (location.pathname === '/') {
-      header = (
-        <h1>
-          <Link to={'/'} >
-            Noah Glusenkamp
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3>
-          <Link to={'/'}>
-            Noah Glusenkamp
-          </Link>
-        </h3>
-      )
-    }
+    console.log(this.props)
+    const {location, children } = this.props
+
+    const AppTitle = styled.h1`
+      font-size: 64px;
+      > a { 
+        text-decoration: none;
+        color: black;
+      }
+    `
+    const AppWrapper = styled.div`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    `
+    const AppMain = styled.section`
+      width: 50%;
+      @media (orientation: portrait) {
+        width: 100%;
+        padding-right: 12px;
+        padding-left: 12px;
+      }
+      
+    `
     return (
-      <div>
-        {header}
-        {children()}
-      </div>
+      <AppWrapper>
+        <AppMain>
+          <AppTitle>
+            <Link to="/">
+              Noah Glusenkamp
+            </Link>
+          </AppTitle>
+          {children()}
+        </AppMain>
+      </AppWrapper>
     )
   }
 }
